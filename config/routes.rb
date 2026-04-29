2fdb3323034079386b054421de9edf554e98beca
David Young <david@dcyoung.dev>
2026-04-29 16:34:36 +0100

    Reordered routes alphabetically

--- config/routes.rb
+++ config/routes.rb
@@ -1,58 +1,58 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: :json } do
+    namespace :lookup_items do
+      resources :countries, only: :index
+      resources :degree_countries, only: :index
+      resources :teaching_subjects, only: :index
+    end
     namespace :pick_list_items do
+      namespace :candidate do
+        resources :adviser_eligibilities, only: :index
+        resources :adviser_requirements, only: :index
+        resources :assignment_statuses, only: :index
+        resources :channels, only: :index
+        resources :citizenships, only: :index
+        resources :consideration_journey_stages, only: :index
+        resources :event_subscription_channels, only: :index
+        resources :gcse_statuses, only: :index
+        resources :has_qualified_teacher_statuses, only: :index
+        resources :initial_teacher_training_years, only: :index
+        resources :locations, only: :index
+        resources :mailing_list_subscription_channels, only: :index
+        resources :preferred_education_phases, only: :index
+        resources :retake_gcse_statuses, only: :index
+        resources :situations, only: :index
+        resources :teacher_training_adviser_subscription_channels, only: :index
+        resources :types, only: :index
+        resources :visa_statuses, only: :index
+      end
       namespace :contact_creation_channel do
         resources :activities, only: :index
         resources :services, only: :index
         resources :sources, only: :index
       end
-      namespace :service_subscription do
-        resources :types, only: :index
+      namespace :past_teaching_position do
+        resources :education_phases, only: :index
       end
       namespace :phone_call do
         resources :channels, only: :index
       end
-      namespace :teaching_event do
-        resources :accessibility_items, only: :index
-        resources :registration_channels, only: :index
-        resources :statuses, only: :index
-        resources :regions, only: :index
-        resources :types, only: :index
-      end
-      namespace :past_teaching_position do
-        resources :education_phases, only: :index
-      end
       namespace :qualification do
-        resources :uk_degree_grades, only: :index
         resources :degree_statuses, only: :index
         resources :types, only: :index
+        resources :uk_degree_grades, only: :index
       end
-      namespace :candidate do
-        resources :has_qualified_teacher_statuses, only: :index
-        resources :locations, only: :index
-        resources :visa_statuses, only: :index
-        resources :citizenships, only: :index
-        resources :situations, only: :index
-        resources :assignment_statuses, only: :index
+      namespace :service_subscription do
         resources :types, only: :index
-        resources :adviser_requirements, only: :index
-        resources :adviser_eligibilities, only: :index
-        resources :consideration_journey_stages, only: :index
-        resources :retake_gcse_statuses, only: :index
-        resources :gcse_statuses, only: :index
-        resources :teacher_training_adviser_subscription_channels, only: :index
-        resources :event_subscription_channels, only: :index
-        resources :mailing_list_subscription_channels, only: :index
-        resources :channels, only: :index
-        resources :preferred_education_phases, only: :index
-        resources :initial_teacher_training_years, only: :index
       end
-    end
-    namespace :lookup_items do
-      resources :countries, only: :index
-      resources :degree_countries, only: :index
-      resources :teaching_subjects, only: :index
+      namespace :teaching_event do
+        resources :accessibility_items, only: :index
+        resources :regions, only: :index
+        resources :registration_channels, only: :index
+        resources :statuses, only: :index
+        resources :types, only: :index
+      end
     end
   end
 
